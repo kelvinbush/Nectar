c9d58ebffc5d15460307ac896c84d627a9d793cf
Kelvin Bush <kelybush@gmail.com>
2021-08-16 17:47:30 +0300

    dding logo image

---
+++ app/src/main/java/com/kelvinbush/nectar/ui/screens/SplashScreen.kt
@@ -0,0 +1,20 @@
+package com.kelvinbush.nectar.ui.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun SplashScreen() {
+    Column {
+        Row {
+            Column {
+
+            }
+            Column {
+
+            }
+        }
+    }
+}
--- app/src/main/java/com/kelvinbush/nectar/ui/theme/Theme.kt
+++ app/src/main/java/com/kelvinbush/nectar/ui/theme/Theme.kt
@@ -5,15 +5,16 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
+    primary = Color(0xff53B175),
     primaryVariant = Purple700,
     secondary = Teal200
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
+    primary = Color(0xff53B175),
     primaryVariant = Purple700,
     secondary = Teal200
 
