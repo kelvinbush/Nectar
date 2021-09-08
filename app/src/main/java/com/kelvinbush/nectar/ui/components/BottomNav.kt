a4a15c836a4e5de06c44551d729a5965db6ff49b
Kelvin Bush <kelybush@gmail.com>
2021-09-08 09:42:27 +0300

    finishing up bottom nav
    Signed-off-by: Kelvin Bush <kelybush@gmail.com>

--- app/src/main/java/com/kelvinbush/nectar/ui/components/BottomNav.kt
+++ app/src/main/java/com/kelvinbush/nectar/ui/components/BottomNav.kt
@@ -1,26 +1,66 @@
 package com.kelvinbush.nectar.ui.components
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.BottomAppBar
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.kelvinbush.nectar.R
 
 @Composable
 fun BottomNav() {
-    Row(
-        horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment = Alignment.CenterVertically,
+    BottomAppBar(
+        elevation = 8.dp,
+        cutoutShape = RoundedCornerShape(
+            topStartPercent = 23,
+            topEndPercent = 23,
+            bottomEndPercent = 0,
+            bottomStartPercent = 0
+        ),
         modifier = Modifier.fillMaxWidth()
     ) {
-        Column() {
-
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column {
+                Icon(painter = painterResource(id = R.drawable.shop), contentDescription = null)
+                Text(text = "Shop")
+            }
+            Column() {
+                Icon(
+                    painter = painterResource(id = R.drawable.explore),
+                    contentDescription = null
+                )
+                Text(text = "Explore")
+            }
+            Column() {
+                Icon(
+                    painter = painterResource(id = R.drawable.cart),
+                    contentDescription = null
+                )
+                Text(text = "Cart")
+            }
+            Column() {
+                Icon(
+                    painter = painterResource(id = R.drawable.favorite),
+                    contentDescription = null
+                )
+                Text(text = "Favourite")
+            }
+            Column() {
+                Icon(
+                    painter = painterResource(id = R.drawable.account),
+                    contentDescription = null
+                )
+                Text(text = "Account")
+            }
         }
-        Column() {}
-        Column() {}
-        Column() {}
-        Column() {}
     }
 }
