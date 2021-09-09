8564538bd97e5bf57729df3a9ee8e87325147778
Kelvin Bush <kelybush@gmail.com>
2021-09-09 08:05:04 +0300

    remove bottom nav component
    Signed-off-by: Kelvin Bush <kelybush@gmail.com>

--- app/src/main/java/com/kelvinbush/nectar/ui/screens/bottomNavigation/BottomNavHost.kt
+++ app/src/main/java/com/kelvinbush/nectar/ui/screens/bottomNavigation/BottomNavHost.kt
@@ -9,6 +9,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.NavHost
@@ -31,7 +32,10 @@ fun BottomNavHost() {
     )
     Scaffold(
         bottomBar = {
-            BottomNavigation(backgroundColor = Color.White) {
+            BottomNavigation(
+                backgroundColor = Color.White,
+                elevation = 8.dp
+            ) {
                 val navBackStackEntry by navController.currentBackStackEntryAsState()
                 val currentDestination = navBackStackEntry?.destination
                 bottomItems.forEach { screen ->
