0a4bf677f7e5b99c62d51de02145ce78f5686b30
Kelvin Bush <kelybush@gmail.com>
2021-09-09 08:07:27 +0300

    remove experimental api
    Signed-off-by: Kelvin Bush <kelybush@gmail.com>

--- app/src/main/java/com/kelvinbush/nectar/ui/screens/bottomNavigation/BottomNavHost.kt
+++ app/src/main/java/com/kelvinbush/nectar/ui/screens/bottomNavigation/BottomNavHost.kt
@@ -1,6 +1,5 @@
 package com.kelvinbush.nectar.ui.screens.bottomNavigation
 
-import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
@@ -16,10 +15,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.google.accompanist.pager.ExperimentalPagerApi
 
-@ExperimentalAnimationApi
-@ExperimentalPagerApi
 @Composable
 fun BottomNavHost() {
     val navController = rememberNavController()
