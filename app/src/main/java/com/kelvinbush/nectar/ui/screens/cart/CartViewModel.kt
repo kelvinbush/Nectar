ebd28560704161c188b57f48e3cd735fbd05c742
Kelvin Bush <kelybush@gmail.com>
2022-01-02 10:11:56 +0300

    refactoring get id Token
    Signed-off-by: Kelvin Bush <kelybush@gmail.com>

---
+++ app/src/main/java/com/kelvinbush/nectar/ui/screens/cart/CartViewModel.kt
@@ -0,0 +1,42 @@
+package com.kelvinbush.nectar.ui.screens.cart
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.kelvinbush.nectar.data.remote.FruityService
+import com.kelvinbush.nectar.domain.model.CartItemList
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class CartViewModel @Inject constructor(
+    private val fruityService: FruityService
+) : ViewModel() {
+
+    private var _cart = MutableLiveData<CartItemList>()
+    val cart: LiveData<CartItemList>
+        get() = _cart
+
+    init {
+        getCartItems()
+    }
+
+    private fun getIdToken(): String {
+        val user = Firebase.auth.currentUser
+        var token = ""
+        user?.getIdToken(true)?.addOnSuccessListener { id ->
+            token = "Bearer ${id.token}"
+        }
+        return token
+    }
+
+
+    private fun getCartItems() = viewModelScope.launch(Dispatchers.IO) {
+        _cart.value = fruityService.getCart(getIdToken())
+    }
+}
--- app/src/main/java/com/kelvinbush/nectar/viewmodel/LoginScreenViewModel.kt
+++ app/src/main/java/com/kelvinbush/nectar/viewmodel/LoginScreenViewModel.kt
@@ -5,14 +5,15 @@ import androidx.lifecycle.*
 import com.google.firebase.auth.AuthCredential
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
-import com.kelvinbush.nectar.domain.model.CartItemList
-import com.kelvinbush.nectar.domain.FruityUser
-import com.kelvinbush.nectar.network.CartAdd
 import com.kelvinbush.nectar.data.remote.FruityService
+import com.kelvinbush.nectar.domain.FruityUser
+import com.kelvinbush.nectar.domain.model.CartItemList
+import com.kelvinbush.nectar.network.CartAdd
 import com.kelvinbush.nectar.network.NetworkProduct
 import com.kelvinbush.nectar.network.RemoveProduct
 import com.kelvinbush.nectar.utils.LoadingState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
@@ -54,20 +55,21 @@ class LoginScreenViewModel @Inject constructor(
         _idToken.value = accessToken
     }
 
-    fun signInWithEmailAndPassword(email: String, password: String) = viewModelScope.launch {
-        try {
-            loadingState.emit(LoadingState.LOADING)
-            val user = Firebase.auth.signInWithEmailAndPassword(email, password).await().user
-            user?.getIdToken(true)?.addOnSuccessListener {
-                _idToken.value = it.token
+    fun signInWithEmailAndPassword(email: String, password: String) =
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                loadingState.emit(LoadingState.LOADING)
+                val user = Firebase.auth.signInWithEmailAndPassword(email, password).await().user
+                user?.getIdToken(true)?.addOnSuccessListener {
+                    _idToken.value = it.token
+                }
+                loadingState.emit(LoadingState.LOADED)
+            } catch (e: Exception) {
+                loadingState.emit(LoadingState.error(e.localizedMessage))
             }
-            loadingState.emit(LoadingState.LOADED)
-        } catch (e: Exception) {
-            loadingState.emit(LoadingState.error(e.localizedMessage))
         }
-    }
 
-    fun signWithCredential(credential: AuthCredential) = viewModelScope.launch {
+    fun signWithCredential(credential: AuthCredential) = viewModelScope.launch(Dispatchers.IO) {
         try {
             loadingState.emit(LoadingState.LOADING)
             val user = Firebase.auth.signInWithCredential(credential).await().user
@@ -81,7 +83,7 @@ class LoginScreenViewModel @Inject constructor(
     }
 
     fun login(accessToken: String) {
-        viewModelScope.launch {
+        viewModelScope.launch(Dispatchers.IO) {
             val token = "Bearer $accessToken"
             Log.d(TAG, token)
             _fUser.value = fruityService.login(token)
@@ -94,7 +96,7 @@ class LoginScreenViewModel @Inject constructor(
             user.getIdToken(true).addOnSuccessListener {
                 _idToken.value = it.token
                 val token = "Bearer ${_idToken.value}"
-                viewModelScope.launch {
+                viewModelScope.launch(Dispatchers.IO) {
                     _products.value = fruityService.getAllProducts(token).result
                 }
             }
@@ -104,34 +106,33 @@ class LoginScreenViewModel @Inject constructor(
     }
 
     fun getCartItems() {
-        viewModelScope.launch {
+        viewModelScope.launch(Dispatchers.IO) {
             val token = "Bearer ${_idToken.value}"
             _cart.value = fruityService.getCart(token)
         }
     }
 
-    fun addCart(id:String, quantity:Int =1 ){
+    fun addCart(id: String, quantity: Int = 1) {
         val user = Firebase.auth.currentUser
         user?.getIdToken(true)?.addOnSuccessListener {
             _idToken.value = it.token
             val token = "Bearer ${_idToken.value}"
             val username = user.uid
-            viewModelScope.launch {
-                fruityService.addToCart(token, CartAdd(username,id, quantity))
+            viewModelScope.launch(Dispatchers.IO) {
+                fruityService.addToCart(token, CartAdd(username, id, quantity))
             }
             Log.d(TAG, "addCart: $id")
         }
     }
 
-    fun removeFromCart(id:String){
+    fun removeFromCart(id: String) {
         val user = Firebase.auth.currentUser
         user?.getIdToken(true)?.addOnSuccessListener {
             _idToken.value = it.token
             val token = "Bearer ${_idToken.value}"
-            viewModelScope.launch {
+            viewModelScope.launch(Dispatchers.IO) {
                 fruityService.deleteFromCart(token, RemoveProduct(id))
             }
         }
     }
-
 }
