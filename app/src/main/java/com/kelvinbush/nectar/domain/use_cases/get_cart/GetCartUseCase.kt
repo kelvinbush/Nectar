b0f27733f6473470e691e95631b8e2929d93a674
Kelvin Bush <kelybush@gmail.com>
2022-01-09 19:05:41 +0300

    update get cart use case
    Signed-off-by: Kelvin Bush <kelybush@gmail.com>

--- app/src/main/java/com/kelvinbush/nectar/domain/use_cases/get_cart/GetCartUseCase.kt
+++ app/src/main/java/com/kelvinbush/nectar/domain/use_cases/get_cart/GetCartUseCase.kt
@@ -3,8 +3,25 @@ package com.kelvinbush.nectar.domain.use_cases.get_cart
 import com.kelvinbush.nectar.domain.FruityRepository
 import com.kelvinbush.nectar.domain.model.CartItemList
 import com.kelvinbush.nectar.domain.model.ShoppingSession
+import com.kelvinbush.nectar.util.Resource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
 
 class GetCartUseCase(private val repository: FruityRepository) {
-    suspend operator fun invoke(authToken: String, sessionId: ShoppingSession): CartItemList =
-        repository.getCart(authToken = authToken, sessionId = sessionId)
+    suspend operator fun invoke(
+        authToken: String,
+        sessionId: ShoppingSession,
+    ): Flow<Resource<CartItemList>> = flow {
+        try {
+            emit(Resource.Loading())
+            val items = repository.getCart(authToken = authToken, sessionId = sessionId)
+            emit(Resource.Success(items))
+        } catch (e: HttpException) {
+            emit(Resource.Error(e.localizedMessage ?: "An unexpected error occurred"))
+        } catch (e: IOException) {
+            emit(Resource.Error("Couldn't reach server. Check your internet connection."))
+        }
+    }
 }
--- app/src/main/java/com/kelvinbush/nectar/presentation/screens/bottom_nav_screens/cart/Cart.kt
+++ app/src/main/java/com/kelvinbush/nectar/presentation/screens/bottom_nav_screens/cart/Cart.kt
@@ -1,16 +1,14 @@
 package com.kelvinbush.nectar.presentation.screens.bottom_nav_screens.cart
 
-import androidx.compose.foundation.BorderStroke
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -20,16 +18,21 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import coil.compose.rememberImagePainter
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.kelvinbush.nectar.R
 import com.kelvinbush.nectar.domain.model.CartProduct
 import com.kelvinbush.nectar.presentation.screens.splash.SplashViewModel
-import com.kelvinbush.nectar.ui.theme.productTextStyle
+import com.kelvinbush.nectar.ui.theme.headerTextStyle
+import com.kelvinbush.nectar.ui.theme.itemNameTextStyle
+import com.kelvinbush.nectar.ui.theme.itemPriceTextStyle
 
 @Composable
 fun MyCart(
@@ -41,99 +44,167 @@ fun MyCart(
     val user by splashViewModel.fUser.observeAsState()
     user?.user?.shoppingSession?.let { cartViewModel.getCartItems(it) }
 
-    LazyColumn() {
-        itemsInCart.cartItems?.forEach {
-            item {
-                CartItem(cartItem = it)
+    val systemUiController = rememberSystemUiController()
+    systemUiController.setSystemBarsColor(
+        color = Color.Transparent,
+        darkIcons = true
+    )
+
+    val context = LocalContext.current
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = "My Cart",
+            style = headerTextStyle,
+            textAlign = TextAlign.Center
+        )
+        Spacer(modifier = Modifier.height(32.dp))
+        Divider(color = Color(0xFFB3B3B3), modifier = Modifier.fillMaxWidth(), thickness = 1.dp)
+        LazyColumn(modifier = Modifier.fillMaxHeight(0.8f)) {
+            itemsInCart.forEach { _ ->
+                item {
+                    CartItem(item = CartProduct(id = "1", 30, product = product))
+                    Divider(
+                        color = Color(0xFFB3B3B3),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp),
+                        thickness = 1.dp
+                    )
+                }
             }
+        }
+//        EmptyCart()
+
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            modifier = Modifier
+                .height(57.dp)
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            onClick = {
+                Toast
+                    .makeText(context, "Go To Checkout", Toast.LENGTH_SHORT)
+                    .show()
+            },
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = Color(0xFF53B175),
+                contentColor = Color(0xFFFFFFFF)
+            ),
+            shape = RoundedCornerShape(19),
+            elevation = ButtonDefaults.elevation(defaultElevation = 4.dp, pressedElevation = 8.dp)
+        ) {
+            Text(
+                text = "Go To Checkout",
+                style = itemPriceTextStyle,
+                color = Color(0xFFFFFFFF)
+            )
         }
     }
 }
 
 
 @Composable
-fun CartItem(cartItem: CartProduct) {
+fun CartItem(item: CartProduct) {
     Row(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.SpaceBetween
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(all = 12.dp)
+            .height(77.dp)
     ) {
         Column(
-            modifier = Modifier.fillMaxHeight(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.Start
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ) {
             Image(
-                painter = rememberImagePainter(data = cartItem.product.imageUrl),
-                contentDescription = stringResource(R.string.cart_item),
+                painter = rememberImagePainter(data = item.product.imageUrl),
+                contentDescription = "image",
                 contentScale = ContentScale.Fit
             )
         }
-        Column {
-            Text(text = cartItem.product.name)
-            Text(text = "Quantity Price")
-            QuantityToggle(
-                minusOne = { /*TODO*/ },
-                plusOne = {},
-                quantity = cartItem.quantity
-            )
+        Column(
+            modifier = Modifier
+                .weight(2f)
+                .padding(start = 16.dp)
+                .fillMaxHeight()
+        ) {
+            Text(text = item.product.name, style = itemNameTextStyle)
+            Text(text = "Kshs, Price", style = MaterialTheme.typography.h5)
+            Spacer(modifier = Modifier.height(16.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(0.68f),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                IconButton(
+                    modifier = Modifier
+                        .size(30.dp)
+                        .border(
+                            shape = RoundedCornerShape(30),
+                            width = 1.dp,
+                            color = Color(0xFFB3B3B3)
+                        ),
+                    enabled = item.quantity > 1,
+                    onClick = { /*TODO*/ }) {
+                    Icon(
+                        modifier = Modifier
+                            .width(17.dp)
+                            .align(Alignment.CenterVertically),
+                        painter = painterResource(id = R.drawable.ic_remove),
+                        contentDescription = "Add Icon",
+                        tint = if (item.quantity > 1) Color(0xFF53B175) else Color(0xFFB3B3B3)
+                    )
+                }
+                Text(
+                    text = "${item.quantity}",
+                    style = MaterialTheme.typography.h6,
+                    textAlign = TextAlign.Center
+                )
+                IconButton(
+                    modifier = Modifier
+                        .size(30.dp)
+                        .border(
+                            shape = RoundedCornerShape(30),
+                            width = 1.dp,
+                            color = Color(0xFFB3B3B3)
+                        ),
+                    onClick = { /*TODO*/ }
+                ) {
+                    Icon(
+                        modifier = Modifier
+                            .size(17.dp)
+                            .align(Alignment.CenterVertically),
+                        imageVector = Icons.Default.Add,
+                        contentDescription = "Add Icon",
+                        tint = Color(0xFF53B175)
+                    )
+                }
+            }
         }
         Column(
-            modifier = Modifier.fillMaxHeight(),
-            verticalArrangement = Arrangement.SpaceBetween
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight(),
+            verticalArrangement = Arrangement.SpaceBetween,
+            horizontalAlignment = Alignment.End
         ) {
             IconButton(onClick = { /*TODO*/ }) {
                 Icon(
+                    modifier = Modifier.size(20.dp),
                     imageVector = Icons.Default.Close,
-                    contentDescription = stringResource(R.string.CLOSE_ICON)
+                    contentDescription = "close icon",
+                    tint = Color(0xFFB3B3B3)
                 )
             }
-            Text(text = "Kshs${cartItem.product.price}")
-        }
-    }
-}
-
-@Composable
-fun QuantityToggle(
-    quantity: Int = 1,
-    minusOne: () -> Unit,
-    plusOne: () -> Unit,
-) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth(.65f)
-            .padding(top = 12.dp),
-        horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        IconButton(
-            onClick = { minusOne() },
-            modifier = Modifier
-                .border(
-                    BorderStroke(1.dp, Color(0xffe2e2e2)),
-                    shape = RoundedCornerShape(33)
-                )
-                .size(30.dp)
-        ) {
-            Icon(
-                painter = painterResource(id = R.drawable.ic_baseline_remove_24),
-                contentDescription = null,
-                tint = Color(0xffb3b3b3)
-            )
-        }
-        Text(text = quantity.toString(), style = productTextStyle)
-        IconButton(
-            onClick = { plusOne() },
-            modifier = Modifier
-                .border(
-                    BorderStroke(1.dp, Color(0xffe2e2e2)),
-                    shape = RoundedCornerShape(33)
-                )
-                .size(30.dp)
-        ) {
-            Icon(
-                painter = painterResource(id = R.drawable.ic_add),
-                contentDescription = null,
-                tint = MaterialTheme.colors.primary
+            Text(
+                text = "Kshs ${item.product.price * item.quantity}",
+                style = itemPriceTextStyle,
+                overflow = TextOverflow.Visible,
+                maxLines = 2
             )
         }
     }
