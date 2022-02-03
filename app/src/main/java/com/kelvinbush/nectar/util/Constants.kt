46eea9be19c353feeb32467754d9186da4552566
Kelvin Bush <kelybush@gmail.com>
2022-02-03 12:17:50 +0300

    adding session manager class
    Signed-off-by: Kelvin Bush <kelybush@gmail.com>

--- app/src/main/java/com/kelvinbush/nectar/util/Constants.kt
+++ app/src/main/java/com/kelvinbush/nectar/util/Constants.kt
@@ -4,4 +4,6 @@ object Constants {
     const val BASE_URL = "https://nectar-api-server.herokuapp.com/"
     const val CART_ARGUMENT_KEY = "sessionId"
     const val DETAIL_ARGUMENT_KEY = "product"
+    const val ACCESS_TOKEN = "accessToken"
+    const val REFRESH_TOKEN = "refreshToken"
 }
